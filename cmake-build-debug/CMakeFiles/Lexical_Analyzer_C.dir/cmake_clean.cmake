be8144a4a0ca5036fc29961b2ac01341bdc67f20
martys34 <spiewakmarty@gmail.com>
2017-11-08 20:34:56 -0500

    c lexical analyzer

---
+++ cmake-build-debug/CMakeFiles/Lexical_Analyzer_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lexical_Analyzer_C.dir/main.c.obj"
+  "Lexical_Analyzer_C.pdb"
+  "Lexical_Analyzer_C.exe"
+  "Lexical_Analyzer_C.exe.manifest"
+  "libLexical_Analyzer_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lexical_Analyzer_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
